ffb597f6905849ec169b29e724b097c9b8c28541
Sarath V <a-8802@LE1219.local>
2020-10-26 15:41:10 +0530

    Calender ID update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const 
   GOOGLE_API_KEY = 'AIzaSyB02EuL6kskKemm_bKcjagGfAPj5MEQV9Q',
-  CALENDAR_ID = 'fnf8qvtpgl1r669lkkuki9b4to@group.calendar.google.com';
+  CALENDAR_ID = 'mk2l0d84ctaejblnr0s34atcjg@group.calendar.google.com';
